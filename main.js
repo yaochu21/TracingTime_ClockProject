ffd31ecaedd905600cc1d8f37b260abd7e1d7211
yaochu21 <yaochu1828@gmail.com>
2023-11-01 01:00:49 -0500

    update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { Layers } from 'three';
 import { Vector3 } from 'three';
 import { DragControls } from 'three/addons/controls/DragControls.js';
-// import {NodeArray,Clock} from 'classes.js';
+//import {NodeArray,Clock} from 'classes.js';
 
 //#region scene setup
 const bgColor = new THREE.Color(0xfefefe);
@@ -23,26 +23,49 @@ document.body.appendChild( renderer.domElement );
 //#endregion
 
 //#region materials
+
+const color_jade = 0x00c480
+const color_black = 0x0
+const color_grey = 0x8a8a8a
+const color_light_grey = 0xb8b8b8
+const color_blue = 0x009fe3
+const color_red = 0xd00000
+
 const material_black = new THREE.MeshBasicMaterial({
-    color: 0x0
+    color: color_black
 });
 
-const material_blue = new THREE.MeshBasicMaterial({
-    color: 0x55ccdd
+const material_jade = new THREE.MeshBasicMaterial({
+    color: color_jade
 })
 
 const material_grey = new THREE.MeshBasicMaterial({
-    color: 0x8a8a8a
+    color: color_grey
+})
+
+const material_light_grey = new THREE.MeshBasicMaterial({
+    color: color_light_grey
 })
 
 const material_bg = new THREE.MeshBasicMaterial({
     color: bgColor,
 })
-const material_line_black = new THREE.LineBasicMaterial( { color: 0x00, linewidth: 0.5 } );
+
+
+
+const material_line_black = new THREE.LineBasicMaterial( { color: color_black, linewidth: 0.5 } );
+
+const material_line_blue = new THREE.LineBasicMaterial( { color: color_blue, linewidth: 0.5 } );
+
+const material_line_red = new THREE.LineBasicMaterial( { color: color_red, linewidth: 0.5 } );
+
+const material_line_jade = new THREE.LineBasicMaterial( { color: color_jade, linewidth: 0.5 } );
+
+
 //#endregion
 
 //#region classes
-function borderedCircle(x,y,radius,thickness=0.5,outline_material=material_black,face_material=material_bg) {
+function clockShape(x,y,radius,thickness=0.5,outline_material=material_black,face_material=material_bg) {
     const circleIn = new THREE.Shape();
     const circleOut = new THREE.Shape();
     circleIn.absarc(0, 0, radius);
@@ -54,7 +77,6 @@ function borderedCircle(x,y,radius,thickness=0.5,outline_material=material_black
     const mesh_1 = new THREE.Mesh(geometry_1, face_material);
     const mesh_2 = new THREE.Mesh(geometry_2, outline_material);
     mesh_1.position.set(x,y,0);
-    mesh_2.position.set(x,y,0);
 
     return [mesh_1,mesh_2]
 }
@@ -88,18 +110,17 @@ class Clock {
         this.speed_second = speed_second;
         this.speed_minute = speed_minute ? speed_minute : this.speed_second / factor;
         this.speed_hour = speed_hour ? speed_hour : this.speed_minute / factor;
-        this.dst_clocks = [];
-        this.src_clocks = [];
+        this.node_links = [];
         this.scene = scene;
         this.Instantiate();
     }
 
     Instantiate() {
-        const [in_circle,out_circle] = borderedCircle(this.pos_x,this.pos_y,this.radius,this.border_width);
-        const hour = arrow(0,0,this.pos_x,this.pos_y,new THREE.Vector3(1,1,0),this.radius / 2);
-        const minute = arrow(0,0,this.pos_x,this.pos_y,new THREE.Vector3(0,1,0),this.radius * 3 / 4);
-        const second = arrow(-this.radius/10,-this.radius/10,this.pos_x,this.pos_y,new THREE.Vector3(1,1,0),this.radius);
-        const dot = circle(this.pos_x,this.pos_y,0.7 * this.radius / 40);
+        const [in_circle,out_circle] = clockShape(this.pos_x,this.pos_y,this.radius,this.border_width);
+        const hour = arrow(0,0,0,0,new THREE.Vector3(1,1,0),this.radius / 2);
+        const minute = arrow(0,0,0,0,new THREE.Vector3(0,1,0),this.radius * 3 / 4);
+        const second = arrow(-this.radius/10,-this.radius/10,0,0,new THREE.Vector3(1,1,0),this.radius);
+        const dot = circle(0,0,0.7 * this.radius / 40);
         dot.renderOrder = 1;
 
         this.hourhand = hour;
@@ -127,27 +148,16 @@ class Clock {
         this.secondhand.rotateZ(this.speed_second);
     }
 
-    Decelerate() {
-
-    }
-
-    Accelerate() {
-
-    }
-
-    Repulsion() {
-
-    }
+    
 }
 
 class NodeArray {
-    constructor(scene,clock,count = 1, offset = 4,size = 1.5, material = material_grey ,material_hover = material_blue, start_angle = 0,) {
+    constructor(scene,clock,count = 1, offset = 4,size = 2, material = material_grey, start_angle = 0,) {
         this.clock = clock;
         this.offset = offset;
         this.count = count;
         this.size = size;
         this.material = material;
-        this.material_hover = material_hover;
         this.start_angle = start_angle;
         this.scene = scene;
         this.Instantiate();
@@ -164,13 +174,83 @@ class NodeArray {
             const mesh = new THREE.Mesh(geometry,this.material);
             mesh.rotateZ(angle_offset*i);
             mesh.userData = this;
-            this.clock.in_circle.add(mesh)
+            mesh.layers.enable(NODELAYER);
+            this.clock.in_circle.add(mesh);
         }
     }
 }
+
+class NodeLink {
+    constructor(scene,node_a,node_b,clock_a,clock_b,func_index=0,func_args=null) {
+
+        this.mats = [material_line_black,material_line_blue,material_line_jade,material_line_red];
+        this.funcs = [this.Default,this.Accelerate,this.Decelerate,this.Repulsion]
+        this.scene = scene;
+
+        this.node_a = node_a;
+        this.node_b = node_b;
+        this.src_clock = clock_a;
+        this.dst_clock = clock_b;
+        this.active_func_index = func_index;
+        this.func_args = func_args;
+
+        console.log(clock_a);
+        clock_a.node_links.push(this);
+
+        this.Instantiate();
+        
+    }
+
+    Instantiate() {
+        console.log(this);
+        const points = [];
+        var pos_a = new Vector3();
+        var pos_b = new Vector3();
+        this.node_a.getWorldPosition(pos_a);
+        this.node_b.getWorldPosition(pos_b);
+
+        const o = new THREE.Vector3( pos_a.x, pos_a.y, 0 )
+        const end = new THREE.Vector3(pos_b.x, pos_b.y, 0)
+        points.push( o );
+        points.push(end);
+
+        const geometry = new THREE.BufferGeometry().setFromPoints( points );
+        const line = new THREE.Line( geometry, this.mats[this.active_func_index] );
+
+        console.log(line);
+
+        this.shape = line;
+        this.scene.add(line);
+    }
+
+    Effect() {
+
+    }
+
+
+    Default(src,dst) {
+
+    }
+    
+    Accelerate(src,dst) {
+    
+    }
+    
+    Decelerate(src,dst) {
+    
+    }
+    
+    Repulsion(src,dst) {
+        
+    }
+
+}
+
+
 //#endregion
 
 var clicked = []
+var hovered = null;
 var NODELAYER = 5;
 const pointer = new THREE.Vector2();
 const nodeRaycaster = new THREE.Raycaster();
@@ -181,31 +261,91 @@ function onPointerMove( event ) {
 	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
+    nodeRaycaster.setFromCamera(pointer,camera);
+    const intersects = nodeRaycaster.intersectObjects(my_scene.children);
+
+    if (intersects.length > 0) {
+        var obj = intersects[0].object;
+        if (clicked.indexOf(obj) == -1) {
+            obj.material = material_light_grey;
+            hovered = obj;
+        }
+    }
+    else {
+        if (hovered) {
+            if (clicked.indexOf(hovered) == -1) {
+                hovered.material = material_grey;
+                hovered = null;
+            }
+        }
+    }
+    
+
 }
 
 function onPointerClick( event ) {
     nodeRaycaster.setFromCamera( pointer, camera );
     const intersects = nodeRaycaster.intersectObjects( my_scene.children );
+
+    console.log(intersects);
+    if (intersects.length <= 0) {
+        if (clicked.length > 0) {
+            for (var i = 0; i < clicked.length; i++) {
+                clicked[i].material = material_grey;
+            }
+            clicked = []
+        }
+        return;
+    }
     
     if (clicked.length >= 2) {
         clicked = []
     }
-    clicked.push(intersects[i]);
-    console.log(clicked);
+    console.log("clicked on node")
+    var obj = intersects[0].object;
+    clicked.push(obj);
+    obj.material = material_black;
+
+    if (clicked.length == 2) {
+
+        processNodeLink(clicked);
+    }
+}
+
+function processNodeLink(nodes) {
+    console.log("processing Node Link")
+    if (clicked.length < 2) {
+        return;
+    }
+    var node_a = nodes[0]
+    var node_b = nodes[1]
+
+    if (node_a.userData == node_b.userData) {
+        console.log("nodes in same node array")
+        return;
+    }
+
+    console.log("instantiating node link")
+    var node_link = new NodeLink(my_scene,node_a,node_b,node_a.userData.clock,node_b.userData.clock);
+
+    
 }
 
 
 //#region main
-const clock = new Clock(my_scene,0,0,40,0.5,10,0.008);
-const node_arr = new NodeArray(my_scene,clock,2);
+const clock_a = new Clock(my_scene,0,0,40,0.5,10,0.008);
+const clock_b = new Clock(my_scene,10,10,40,0.5,10,0.01);
+const node_arr_a = new NodeArray(my_scene,clock_a,4);
+const node_arr_b = new NodeArray(my_scene,clock_b,4);
 
-const controls = new DragControls( [clock.in_circle], camera, renderer.domElement );
+const controls = new DragControls( [clock_a.in_circle,clock_b.in_circle], camera, renderer.domElement );
 controls.recursive = false;
 
 function draw() {
 	requestAnimationFrame( draw );
 
-    clock.Tick();
+    clock_a.Tick();
+    clock_b.Tick();
 
 	renderer.render( my_scene, camera );
 }
